6a8ff1832d5051dfacd5b5c474e3c6ebf934e639
shchang <shchang@linkedin.com>
2021-08-19 16:22:25 -0700

    update

--- singleton.hpp
+++ singleton.hpp
@@ -1,10 +1,35 @@
 #pragma once
+
 #include "lrucache.h"
 #include "scale-lrucache.h"
 
+namespace sentinel {
 
-using SoftIpCache = LRUC::ScalableLRUCache<int, int>;
+enum class CACHE_VALUE_TYPE {
+    TIME_ENTITY_LOOKUP_INFO,
+}
+
+template<CACHE_VALUE_TYPE E = TIME_ENTITY_LOOKUP_INFO>
+struct CacheValue {
+    int64_t expiryTs;
+    int denialInfoCode;
+    int routingPrefixSize;
+    bool requiresGoodBotUserAgent;
+
+    CacheValue() = default;
+    CacheValue(int64_t ts, int infoCode = 0, int prefixSize = 32, bool requiresGoodUserAgent = false) :
+    expiryTs(ts),
+    denialInfoCode(infoCode),
+    routingPrefixSize(prefixSize),
+    requiresGoodBotUserAgent(requiresGoodUserAgent) { };
+};
+
+using SoftIpCache = LRUC::ScalableLRUCache<int, CacheValue<>>;
+
+} // namespace sentinel
+
+SoftIpCache* GetSoftIpCache();
 
 extern "C" {
-   SoftIpCache* GetSoftIpCache();
+    void* GetSoftIpCache();
 }
